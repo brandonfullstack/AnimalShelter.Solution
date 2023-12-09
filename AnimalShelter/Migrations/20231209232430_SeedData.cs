a1143e8f991ba8854a730088cdeaecad6c629637
Brandon Wright <brandonfullstack@gmail.com>
2023-12-09 18:25:16 -0500

    Add seed data and update database

---
+++ AnimalShelter/Migrations/20231209232430_SeedData.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AnimalShelter.Migrations
+{
+    public partial class SeedData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Animals",
+                columns: new[] { "AnimalId", "Age", "Color", "Name", "Species" },
+                values: new object[,]
+                {
+                    { 1, 2, "Black & Tan", "Beethoven", "Dog" },
+                    { 2, 3, "Orange", "Garfield", "Cat" },
+                    { 3, 4, "Tan", "Marley", "Dog" },
+                    { 4, 1, "Brindle", "Fido", "Dog" },
+                    { 5, 4, "Grey", "Smokey", "Cat" },
+                    { 6, 1, "Black", "Cujo", "Dog" },
+                    { 7, 2, "Calico", "Cali", "Cat" },
+                    { 8, 6, "Golden", "Bud", "Dog" },
+                    { 9, 1, "Red", "Clifford", "Dog" },
+                    { 10, 4, "Tuxedo", "Tom", "Cat" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 7);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 8);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 9);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 10);
+        }
+    }
+}
--- AnimalShelter/Models/AnimalShelterContext.cs
+++ AnimalShelter/Models/AnimalShelterContext.cs
@@ -9,6 +9,22 @@ namespace AnimalShelter.Models
     public AnimalShelterContext(DbContextOptions<AnimalShelterContext> options) : base(options)
     {
     }
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+      builder.Entity<Animal>()
+        .HasData(
+          new Animal { AnimalId = 1, Name = "Beethoven", Species = "Dog", Color = "Black & Tan", Age = 2 },
+          new Animal { AnimalId = 2, Name = "Garfield", Species = "Cat", Color = "Orange", Age = 3 },
+          new Animal { AnimalId = 3, Name = "Marley", Species = "Dog", Color = "Tan", Age = 4 },
+          new Animal { AnimalId = 4, Name = "Fido", Species = "Dog", Color = "Brindle", Age = 1 },
+          new Animal { AnimalId = 5, Name = "Smokey", Species = "Cat", Color = "Grey", Age = 4 },
+          new Animal { AnimalId = 6, Name = "Cujo", Species = "Dog", Color = "Black", Age = 1 },
+          new Animal { AnimalId = 7, Name = "Cali", Species = "Cat", Color = "Calico", Age = 2 },
+          new Animal { AnimalId = 8, Name = "Bud", Species = "Dog", Color = "Golden", Age = 6 },
+          new Animal { AnimalId = 9, Name = "Clifford", Species = "Dog", Color = "Red", Age = 1 },
+          new Animal { AnimalId = 10, Name = "Tom", Species = "Cat", Color = "Tuxedo", Age = 4 }
+        );
+    }
   }
 }
 
